240b17862c6e81fc260062363a8b91c70a821dc8
Sohjiro <felipe@makingdevs.com>
2017-07-27 12:32:02 -0500

    upgrading application version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Todo.Mixfile do
 
   def project do
     [app: :todo,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
